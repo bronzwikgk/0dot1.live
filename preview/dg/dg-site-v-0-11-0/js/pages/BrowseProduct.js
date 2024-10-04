babb1e1f7f3177787177268d6f7670db6ae13fc8
Abhishek Agrawal <ashu.agrawalksj@gmail.com>
2024-10-04 15:20:20 +0530

    updated product gallery

--- preview/dg/dg-site-v-0-11-0/js/pages/BrowseProduct.js
+++ preview/dg/dg-site-v-0-11-0/js/pages/BrowseProduct.js
@@ -5,18 +5,54 @@ import { ProductService } from '../services/ProductService.js';
 import { CartService } from '../services/CartService.js';
 
 // Define page size globally or pass it as a parameter
-let products = [];
+let products =  [
+      {
+        "name": "ITIL®4 Create, Deliver and Support",
+        "description": "ITIL® 4 Create, Deliver and Support addresses the...",
+        "truncatedDescription": "ITIL® 4 Create, Deliver and Support addresses the...",
+        "image": "../assets/assets_dg-site/logo/new logo for cources/ITIL_ATO logo.png",
+        "productUrl": "./productPage-ITIL®4CreateDeliverandSupport.html?productId=66eb5723c40e0b0b76fe0a3e",
+        "_id": "66eb5723c40e0b0b76fe0a3e"
+      },
+      {
+        "name": "SDI – Service Desk Analyst",
+        "description": "The Service Desk Institute (SDI) ® is the leading...",
+        "truncatedDescription": "The Service Desk Institute (SDI) ® is the leading...",
+        "image": "../assets/assets_dg-site/logo/new logo for cources/MSP_ATO_Logo.png",
+        "productUrl": "./productPage-SDI–ServiceDeskAnalyst.html?productId=66eb5723c40e0b0b76fe0a48",
+        "_id": "66eb5723c40e0b0b76fe0a48"
+      },
+      {
+        "name": "Agile Scrum Workshop",
+        "description": "An in-depth workshop on Agile and Scrum methodolog...",
+        "truncatedDescription": "An in-depth workshop on Agile and Scrum methodolog...",
+        "image": "../assets/assets_dg-site/logo/new logo for cources/PRINCE2Agile_ATO logo.png",
+        "productUrl": "./productPage-AgileScrumWorkshop.html?productId=66eb5723c40e0b0b76fe0a39",
+        "_id": "66eb5723c40e0b0b76fe0a39"
+      },
+      {
+        "name": "MSP ® 5 Foundation",
+        "description": "MSP (Managing Successful Programmes), 5th edition...",
+        "truncatedDescription": "MSP (Managing Successful Programmes), 5th edition...",
+        "image": "../assets/assets_dg-site/logo/new logo for cources/MSP_ATO_Logo.png",
+        "productUrl": "./productPage-MSP®5Foundation.html?productId=66eb5723c40e0b0b76fe0a44",
+        "_id": "66eb5723c40e0b0b76fe0a44"
+      }
+    ]
+  
+  
+    ;
 const pageSize = 6;
 let currentPage = 1;
 let totalPages = 1;
 
 // Fetch products from the backend for a specific page
-async function fetchProducts(page = 1, query = '',categories=[]) {
+async function fetchProducts(page = 1, query = '', categories = []) {
     try {
         console.log('Query:', query);
 
         // Call the ProductService to get products with search and pagination
-        const data = await ProductService.getProducts({ page, limit: pageSize, query ,categories});
+        const data = await ProductService.getProducts({ page, limit: pageSize, query, categories });
 
         console.log("Fetching Products");
 
@@ -83,13 +119,14 @@ const getSelectedCategories = () => {
         .map(checkbox => checkbox.value);
 };
 
-    // Handle category filter changes (on checkbox click)
-    categoryCheckboxes.forEach(checkbox => {
-        checkbox.addEventListener('change', () => {
-            fetchProducts(currentPage,null,getSelectedCategories())
-        });
+// Handle category filter changes (on checkbox click)
+categoryCheckboxes.forEach(checkbox => {
+    checkbox.addEventListener('change', () => {
+        fetchProducts(currentPage, null, getSelectedCategories())
     });
+});
 
 
 // Initial fetch of products
-fetchProducts();
+// fetchProducts();
+renderProducts()
