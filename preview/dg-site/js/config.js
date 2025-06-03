320ec1e166e4dc48fe24b9d4a7941f6caf7b4651
Abhishek Agrawal <ashu.agrawalksj@gmail.com>
2025-06-03 16:32:49 +0530

    updated link

--- preview/dg-site/js/config.js
+++ preview/dg-site/js/config.js
@@ -1,5 +1,7 @@
 // config.js
-const BASE_URL = "https://unbelong.in";
+// const BASE_URL = "https://unbelong.in";
+const BASE_URL = "https://unbelong.in/test";
+
 // const BASE_URL = "http://localhost:3000";
 const LMS_BASE_URL = "https://lmsback.ducisgroup.com/test/api";
 export default BASE_URL;
