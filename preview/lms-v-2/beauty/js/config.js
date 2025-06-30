6f810a91b39db36e0a47c662b4d02ffb2f4b7ad2
Abhishek Agrawal <ashu.agrawalksj@gmail.com>
2025-06-30 11:32:43 +0530

    update

--- preview/lms-v-2/beauty/js/config.js
+++ preview/lms-v-2/beauty/js/config.js
@@ -1,5 +1,5 @@
 // config.js
-const BASE_URL = "https://lmsback.ducisgroup.com/test/api";
+const BASE_URL = "https://saya.unbelong.in/api";
 // const BASE_URL = "http://localhost:3000/api";
 // const BASE_URL = "https://test.unbelong.in/api";
 
